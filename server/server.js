5fd9c6ae030e56060cac420dfa2b22f3bd9f5e9d
Aleksa Tonic <morwors@gmail.com>
2019-02-04 14:32:33 +0100

    add newMessage and createMessage

--- server/server.js
+++ server/server.js
@@ -15,6 +15,17 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
 	console.log('New user connected');
 
+	socket.emit('newMessage',{
+		from: 'Jablintski',
+		text: 'See yaaa',
+		createdAt:123
+	});
+	socket.on('createMessage',(message)=>{
+		console.log('createMessage',message);
+	});
+	socket.on('createEmail',(newEmail)=>{
+		console.log('createEmail',newEmail);
+	});
 	socket.on('disconnect',()=>{
 		console.log('User disconnected');
 	});
