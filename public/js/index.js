909cf7b112c9acff875b242f4ba327634030acee
Aleksa Tonic <morwors@gmail.com>
2019-02-05 13:03:27 +0100

    design better

--- public/js/index.js
+++ public/js/index.js
@@ -32,11 +32,12 @@ socket.on('newLocationMessage',function(message){
 
 jQuery('#message-form').on('submit',function(e){
 	e.preventDefault();
-
+	var messageTextbox=jQuery('[name=message]');
 	socket.emit('createMessage',{
 		from:'User',
-		text:jQuery('[name=message]').val()
+		text:messageTextbox.val()
 	},function(){
+		messageTextbox.val('');
 
 	});
 });
@@ -49,13 +50,16 @@ locationButton.on('click',function(){
 		return alert('Geolocation not supported by your browser');
 	}
 
-	navigator.geolocation.getCurrentPosition(function(position){
+	locationButton.attr('disabled','disabled').text('Sending Location...');
 
+	navigator.geolocation.getCurrentPosition(function(position){
+		locationButton.removeAttr('disabled').text('Send Location');
 		socket.emit('createLocationMessage',{
 			latitude:position.coords.latitude,
 			longitude:position.coords.longitude
-		})
+		});
 	},function(){
+		locationButton.removeAttr('disabled').text('Send Location');
 		alert('Unable to fetch location');
 	});
 });
